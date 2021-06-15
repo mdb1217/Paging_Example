d588d44f81c7e98481a7ce15fd74ef5fa167989e
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-06-16 02:27:46 +0900

    [ADD] add load state adapter

--- app/src/main/java/org/sopt/pagingexample/presentation/search/SearchFragment.kt
+++ app/src/main/java/org/sopt/pagingexample/presentation/search/SearchFragment.kt
@@ -1,24 +1,25 @@
 package org.sopt.pagingexample.presentation.search
 
 import android.view.KeyEvent
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.DiffUtil
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.sopt.pagingexample.R
 import org.sopt.pagingexample.databinding.FragmentSearchBinding
 import org.sopt.pagingexample.presentation.base.BaseFragment
 import org.sopt.pagingexample.presentation.search.adapter.DogListAdapter
+import org.sopt.pagingexample.presentation.search.adapter.DogLoadStateAdapter
 import org.sopt.pagingexample.presentation.viewmodel.MainViewModel
 
 class SearchFragment : BaseFragment<FragmentSearchBinding, MainViewModel>() {
     override val layoutResourceId: Int
         get() = R.layout.fragment_search
-    override val viewModel: MainViewModel by viewModels()
+    override val viewModel: MainViewModel by viewModel()
     private val dogListAdapter = DogListAdapter()
 
     override fun initView() {
+        initAdapter()
         initSearchEvent()
     }
 
@@ -32,6 +33,11 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, MainViewModel>() {
                 return@setOnKeyListener false;
             }
         }
+    }
+
+    private fun initAdapter() {
+        binding.rvSearch.adapter = dogListAdapter
+        dogListAdapter.withLoadStateHeaderAndFooter(header = DogLoadStateAdapter(), footer = DogLoadStateAdapter())
     }
 
     private fun getDog(path : String) {
---
+++ app/src/main/java/org/sopt/pagingexample/presentation/search/adapter/DogLoadStateAdapter.kt
@@ -0,0 +1,40 @@
+package org.sopt.pagingexample.presentation.search.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.pagingexample.R
+import org.sopt.pagingexample.databinding.ItemLoadStateBinding
+
+// Adapter that displays a loading spinner when
+// state = LoadState.Loading, and an error message and retry
+// button when state is LoadState.Error.
+class DogLoadStateAdapter : LoadStateAdapter<DogLoadStateAdapter.LoadStateViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        loadState: LoadState
+    ) = LoadStateViewHolder(parent)
+
+    override fun onBindViewHolder(
+        holder: LoadStateViewHolder,
+        loadState: LoadState
+    ) = holder.bind(loadState)
+
+    class LoadStateViewHolder(
+        parent: ViewGroup
+    ) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_load_state, parent, false)
+    ) {
+        private val binding = ItemLoadStateBinding.bind(itemView)
+
+        fun bind(loadState: LoadState) {
+            binding.pbCircle.isVisible = loadState is LoadState.Loading
+        }
+    }
+}
+
+
