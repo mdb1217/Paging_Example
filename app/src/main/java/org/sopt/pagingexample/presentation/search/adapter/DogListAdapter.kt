d5ced883e60b6df8f70bb94f3f7da59922e97abc
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-06-16 02:27:21 +0900

    [ADD] add dog list adapter's diffutil

--- app/src/main/java/org/sopt/pagingexample/presentation/search/adapter/DogListAdapter.kt
+++ app/src/main/java/org/sopt/pagingexample/presentation/search/adapter/DogListAdapter.kt
@@ -8,15 +8,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import org.sopt.pagingexample.databinding.ItemDogListBinding
 
-class DogListAdapter(diffCallback: DiffUtil.ItemCallback<String>) :
-    PagingDataAdapter<String, DogListAdapter.DogListViewHolder>(diffCallback) {
-    private val _data = mutableListOf<String>()
-    var data : List<String> = _data
-        set(value) {
-            _data.clear()
-            _data.addAll(value)
-            notifyDataSetChanged()
+class DogListAdapter : PagingDataAdapter<String, DogListAdapter.DogListViewHolder>(DogDiffUtil) {
+
+    object DogDiffUtil : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
         }
+
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogListViewHolder {
         val binding: ItemDogListBinding = ItemDogListBinding.inflate(
@@ -27,17 +29,16 @@ class DogListAdapter(diffCallback: DiffUtil.ItemCallback<String>) :
     }
 
     override fun onBindViewHolder(holder: DogListViewHolder, position: Int) {
-        holder.bind(_data[position])
+        getItem(position)?.let { holder.bind(it, position) }
     }
 
-    override fun getItemCount(): Int = _data.size
-
-    inner class DogListViewHolder(private val binding: ItemDogListBinding) :
+    class DogListViewHolder(private val binding: ItemDogListBinding) :
             RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(imgDog: String) {
+        fun bind(imgDog: String, position: Int) {
             binding.apply {
                 Glide.with(ivDog.context).load(imgDog).into(ivDog)
+                tvNum.text = position.toString()
             }
         }
     }
