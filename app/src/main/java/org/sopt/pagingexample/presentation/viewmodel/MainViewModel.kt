3aad189a83b47c3ac425ed463de5cb90c8ef9c37
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-06-15 20:36:56 +0900

    [ADD] init main viewmodel

--- app/src/main/java/org/sopt/pagingexample/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/org/sopt/pagingexample/presentation/viewmodel/MainViewModel.kt
@@ -1,6 +1,12 @@
 package org.sopt.pagingexample.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import kotlinx.coroutines.flow.Flow
+import org.sopt.pagingexample.data.repository.DogRepository
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val repo: DogRepository) : ViewModel() {
+    fun getDog(path : String) : Flow<PagingData<String>> = repo.getDog(path).cachedIn(viewModelScope)
 }
